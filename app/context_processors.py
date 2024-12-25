8dc520b6832eb744023111059f8f4350a55ebc8c
Ivan-Skorokhodov <dreikonline5554545@gmail.com>
2024-12-25 23:07:31 +0300

    fixed cache duration

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,10 +1,11 @@
 from django.conf import settings
-import requests
+from django.utils.timezone import now
+from datetime import timedelta
 from app import models
 import jwt
 import time
 from django.core.cache import cache
-from django.db.models import Count
+from django.db.models import Count, Q
 
 
 def get_centrifugo_token(user_id):
@@ -15,10 +16,18 @@ def get_centrifugo_token(user_id):
 
 
 def cache_popular_tags():
+    three_months_ago = now() - timedelta(days=90)
+
     cache_key = "popular_tags"
     cached_popular_tags = models.Tag.objects.annotate(
-        num_questions=Count('question')).order_by('-num_questions')[:10]
-    cache.set(cache_key, cached_popular_tags, 60)
+        num_questions=Count(
+            'question',
+            filter=Q(question__created_at__gte=three_months_ago)
+        )
+    ).order_by('-num_questions')[:10]
+
+    cache_duration = 90 * 24 * 60 * 60  # 90 дней
+    cache.set(cache_key, cached_popular_tags, cache_duration)
 
 
 def get_popular_tags():
@@ -28,10 +37,22 @@ def get_popular_tags():
 
 
 def cache_best_members():
+    one_week_ago = now() - timedelta(days=7)
+
     cache_key = "best_members"
     cached_best_members = models.Profile.objects.annotate(
-        num_answers=Count('answer')).order_by('-num_answers')[:10]
-    cache.set(cache_key, cached_best_members, 60)
+        num_popular_questions=Count(
+            'question',
+            filter=Q(
+                question__created_at__gte=one_week_ago
+            ) & Q(
+                question__answer__isnull=False
+            )
+        )
+    ).order_by('-num_popular_questions')[:10]
+
+    cache_duration = 7 * 24 * 60 * 60  # 7 дней
+    cache.set(cache_key, cached_best_members, cache_duration)
 
 
 def get_best_members():
